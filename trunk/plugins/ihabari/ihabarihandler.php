6f755107437334f31bd52655945e9702d28b188a
linghuazh <linghuazh@16e3aa00-9220-11de-9b7d-c11b5a55d12f>
2009-08-26 09:38:53 +0000

    Add configuration for theme selection

--- trunk/plugins/ihabari/ihabarihandler.php
+++ trunk/plugins/ihabari/ihabarihandler.php
@@ -3,7 +3,8 @@ class iHabariHandler extends UserThemeHandler
 {
 	public function setup_theme()
 	{
-		$this->theme = Themes::create('ihabari', 'RawPHPEngine');
+		$theme_name = Plugins::filter( 'iHabari_theme_name' );
+		$this->theme = Themes::create($theme_name, 'RawPHPEngine');
 		$this->theme->assign('matched_rule', URL::get_matched_rule());
 		$request = new StdClass();
 		foreach(RewriteRules::get_active() as $rule) {
